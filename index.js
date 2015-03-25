1d90578dd3d9db1a85e3693e87e253687da6c94b
Andras <andras@acm.org>
2015-03-25 17:45:08 -0400

    clean up install/uninstall; 1.4.1

--- index.js
+++ index.js
@@ -6,45 +6,24 @@
 module.exports = require('./lib/qtimers.js');
 
 
-var version = process.version.split('.');
-var existingCalls = null;
-// node v0.8 and before did not have setImmediate et al
-if (version[0] == 0 && version[1] < 10) existingCalls = {
-    setImmediate: setImmediate,
-    clearImmediate: clearImmediate,
-    setTimeout: setTimeout,
-    clearTimeout: clearTimeout,
-    setInterval: setInterval,
-    clearInterval: clearInterval,
-    currentTimestamp: global.currentTimestamp,
+existingCalls = {
+    setImmediate: global.setImmediate || false,
+    clearImmediate: global.clearImmediate || false,
+    setTimeout: global.setTimeout || false,
+    clearTimeout: global.clearTimeout || false,
+    setInterval: global.setInterval || false,
+    clearInterval: global.clearInterval || false,
+    currentTimestamp: global.currentTimestamp || false,
 };
 
 module.exports.install = function( ) {
     var i;
-    if (existingCalls) for (i in existingCalls) global[i] = module.exports[i];
-    else {
-        setImmediate = module.exports.setImmediate;
-        clearImmediate = module.exports.clearImmediate;
-        setTimeout = module.exports.setTimeout;
-        clearTimeout = module.exports.clearTimeout;
-        setInterval = module.exports.setInterval;
-        clearInterval = module.exports.clearInterval;
-        currentTimestamp = module.exports.currentTimestamp;
-    }
+    for (i in existingCalls) global[i] = module.exports[i];
 };
 
 module.exports.uninstall = function( ) {
     var i;
-    if (existingCalls) for (i in existingCalls) global[i] = existingCalls[i];
-    else {
-        setImmediate = undefined;
-        clearImmediate = undefined;
-        setTimeout = undefined;
-        clearTimeout = undefined;
-        setInterval = undefined;
-        clearInterval = undefined;
-        currentTimestamp = undefined;
-    }
+    for (i in existingCalls) global[i] = existingCalls[i];
 };
 
 // the default is to install our calls in place of the builtins
