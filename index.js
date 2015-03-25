7e0e264ecf4a01764b80e25d2ff24fa8f76d4516
Andras <andras@acm.org>
2015-03-25 17:02:39 -0400

    changes to work with node-v0.8; 1.4.0 (untested)

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
 module.exports = require('./lib/qtimers.js');
 
 
-var existingCalls = {
+var version = process.version.split('.');
+var existingCalls = null;
+// node v0.8 and before did not have setImmediate et al
+if (version[0] == 0 && version[1] < 10) existingCalls = {
     setImmediate: setImmediate,
     clearImmediate: clearImmediate,
     setTimeout: setTimeout,
@@ -18,12 +21,30 @@ var existingCalls = {
 
 module.exports.install = function( ) {
     var i;
-    for (i in existingCalls) global[i] = module.exports[i];
+    if (existingCalls) for (i in existingCalls) global[i] = module.exports[i];
+    else {
+        setImmediate = module.exports.setImmediate;
+        clearImmediate = module.exports.clearImmediate;
+        setTimeout = module.exports.setTimeout;
+        clearTimeout = module.exports.clearTimeout;
+        setInterval = module.exports.setInterval;
+        clearInterval = module.exports.clearInterval;
+        currentTimestamp = module.exports.currentTimestamp;
+    }
 };
 
 module.exports.uninstall = function( ) {
     var i;
-    for (i in existingCalls) global[i] = existingCalls[i];
+    if (existingCalls) for (i in existingCalls) global[i] = existingCalls[i];
+    else {
+        setImmediate = undefined;
+        clearImmediate = undefined;
+        setTimeout = undefined;
+        clearTimeout = undefined;
+        setInterval = undefined;
+        clearInterval = undefined;
+        currentTimestamp = undefined;
+    }
 };
 
 // the default is to install our calls in place of the builtins
